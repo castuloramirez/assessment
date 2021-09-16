c9c5de0038cd8e03570605be2c98a0e2d0c32da9
Castulo Ramirez <jrami278@jci.com>
2021-09-16 15:25:08 +0200

    Database Refactor

--- database.sql
+++ database.sql
@@ -18,6 +18,30 @@ USE `codereview`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `article_text`
+--
+
+DROP TABLE IF EXISTS `article_text`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article_text` (
+  `id_article` int DEFAULT NULL,
+  `text` text,
+  KEY `id_article` (`id_article`),
+  CONSTRAINT `article_text_ibfk_1` FOREIGN KEY (`id_article`) REFERENCES `articles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article_text`
+--
+
+LOCK TABLES `article_text` WRITE;
+/*!40000 ALTER TABLE `article_text` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article_text` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `articles`
 --
 
@@ -41,6 +65,32 @@ LOCK TABLES `articles` WRITE;
 /*!40000 ALTER TABLE `articles` DISABLE KEYS */;
 /*!40000 ALTER TABLE `articles` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_article` int DEFAULT NULL,
+  `autor` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_article` (`id_article`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`id_article`) REFERENCES `articles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -51,4 +101,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-16 11:15:44
+-- Dump completed on 2021-09-16 15:22:40
